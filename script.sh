b4a5140862dae0e69f06486096977a66a12c6835
jake <jake@webtool.autrui.com>
2017-04-02 13:13:08 -0400

    wrote out config

--- script.sh
+++ script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 IMAGE='diginc/pi-hole:debian'
-BASE_DIR='/home/jake/webtol/config'
+CONF_DIR='/home/jake/webtool/config'
+LOG_DIR='/home/jake/logs'
 
 IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
 IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
-docker run -p 53:53/tcp -p 53:53/udp -p 80:80 --cap-add=NET_ADMIN -e ServerIP="$IP" --restart=always --name pihole -d $IMAGE
+touch $LOG_DIR/pihole.log
+docker run -p 53:53/tcp -p 53:53/udp -p 80:80 -v $LOG_DIR/pihole.log:/var/log/pihole.log -v $CONF_DIR/pihole:/etc/pihole/ --cap-add=NET_ADMIN -e ServerIP="$IP" -e WEBPASSWORD="admin" -e DNS1="10.10.22.2" -e DNS2="10.10.21.2" --restart=always --name pihole -d $IMAGE
 
